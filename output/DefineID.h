611fb89dba08fed32e67746dafae0a823dbd2daa
Your Name <you@example.com>
2020-12-05 08:09:04 +0800

    TFT PROJECT

---
+++ output/DefineID.h
@@ -0,0 +1,363 @@
+//创建画面主界面的命令索引ID
+#define  _SCREEN_主界面                                                           0
+
+//创建画面实验界面的命令索引ID
+#define  _SCREEN_实验界面                                                          1
+
+//创建画面设置界面的命令索引ID
+#define  _SCREEN_设置界面                                                          2
+
+//创建画面历史数据1的命令索引ID
+#define  _SCREEN_历史数据1                                                         3
+
+//创建画面历史数据2的命令索引ID
+#define  _SCREEN_历史数据2                                                         4
+
+//创建画面语言设置的命令索引ID
+#define  _SCREEN_语言设置                                                          5
+
+//创建画面清除所有数据警告的命令索引ID
+#define  _SCREEN_清除所有数据警告                                                      6
+
+//创建画面帮助-关于我们的命令索引ID
+#define  _SCREEN_帮助-关于我们                                                       7
+
+//创建画面内存溢出警告的命令索引ID
+#define  _SCREEN_内存溢出警告                                                        8
+
+//创建画面帮助-帮助文档的命令索引ID
+#define  _SCREEN_帮助-帮助文档                                                       9
+
+//创建画面帮助-联系我们的命令索引ID
+#define  _SCREEN_帮助-联系我们                                                      10
+
+//创建画面清除单次数据警告的命令索引ID
+#define  _SCREEN_清除单次数据警告                                                     11
+
+//创建画面数据导出提示界面的命令索引ID
+#define  _SCREEN_数据导出提示界面                                                     12
+
+#define  _BTN_主界面_BUTTON1                                                      1
+
+#define  _RTC_主界面_RTC1                                                        11
+
+//全局图片主界面v1.3-按下.jpg
+#define  _IMG_主界面V1.3-按下_JPG                                                   1
+
+#define  _BTN_主界面_BUTTON2                                                      2
+
+#define  _BTN_主界面_BUTTON3                                                      3
+
+#define  _BTN_主界面_BUTTON4                                                      4
+
+#define  _BTN_主界面_BUTTON5                                                      5
+
+#define  _BTN_实验界面_BUTTON1                                                     1
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY1                                         45
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY2                                         46
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY3                                         47
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY4                                         48
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY5                                         49
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY6                                         50
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY7                                         51
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY8                                         52
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY9                                         53
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY10                                        54
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY11                                        55
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY12                                        56
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY13                                        57
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY14                                        58
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY15                                        59
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY16                                        60
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY17                                        61
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY18                                        62
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY19                                        63
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY20                                        64
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY21                                        65
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY22                                        66
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY23                                        67
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY24                                        68
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY25                                        69
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY26                                        70
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY27                                        71
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY28                                        72
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY29                                        73
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY30                                        74
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY31                                        77
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY32                                        80
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY33                                        81
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY34                                        82
+
+#define  _BTN_实验界面_BUTTON2                                                    83
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY37                                        88
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY36                                        87
+
+#define  _BTN_实验界面_BUTTON3                                                    89
+
+#define  _BTN_实验界面_BUTTON4                                                    90
+
+#define  _BTN_实验界面_BUTTON5                                                    91
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY38                                        92
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY35                                        86
+
+#define  _RTC_实验界面_RTC1                                                        6
+
+#define  _TXT_DIS__实验界面_TEXT_DISPLAY40                                        93
+
+#define  _BTN_设置界面_BUTTON1                                                     1
+
+#define  _RTC_设置界面_RTC1                                                        2
+
+#define  _PROGRESS_设置界面_PROGRESS1                                              3
+
+#define  _SLIDER_设置界面_SLIDER1                                                  4
+
+#define  _TXT_DIS__设置界面_TEXT_DISPLAY2                                          6
+
+#define  _BTN_设置界面_BUTTON2                                                    10
+
+#define  _BTN_历史数据1_BUTTON1                                                    1
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY3                                         4
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY4                                         5
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY6                                         7
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY8                                         9
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY10                                       11
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY12                                       13
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY15                                       16
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY18                                       19
+
+#define  _BTN_历史数据1_BUTTON2                                                   22
+
+#define  _BTN_历史数据1_BUTTON3                                                   23
+
+#define  _BTN_历史数据1_BUTTON5                                                   25
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY16                                       17
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY5                                         6
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY7                                         8
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY9                                        10
+
+#define  _TXT_DIS__历史数据1_TEXT_DISPLAY11                                       12
+
+#define  _BTN_历史数据1_BUTTON6                                                   27
+
+#define  _BTN_历史数据1_BUTTON7                                                   28
+
+#define  _BTN_历史数据1_BUTTON8                                                   29
+
+#define  _BTN_历史数据1_BUTTON9                                                   30
+
+#define  _BTN_历史数据1_BUTTON10                                                  31
+
+#define  _RTC_历史数据1_RTC1                                                      18
+
+#define  _BTN_历史数据2_BUTTON1                                                    1
+
+#define  _RTC_历史数据2_RTC1                                                       2
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY1                                        48
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY2                                        49
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY3                                        50
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY4                                        51
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY5                                        52
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY6                                        53
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY7                                        54
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY8                                        55
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY9                                        56
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY10                                       57
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY11                                       58
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY12                                       59
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY13                                       60
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY14                                       61
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY15                                       62
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY16                                       63
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY17                                       64
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY18                                       65
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY19                                       66
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY20                                       67
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY21                                       68
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY22                                       69
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY23                                       70
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY24                                       71
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY25                                       72
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY26                                       73
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY27                                       74
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY28                                       75
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY29                                       76
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY30                                       77
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY31                                       78
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY32                                       79
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY33                                       80
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY34                                       81
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY35                                       82
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY36                                       83
+
+#define  _TXT_DIS__历史数据2_TEXT_DISPLAY37                                       84
+
+#define  _BTN_历史数据2_BUTTON2                                                   86
+
+#define  _BTN_历史数据2_BUTTON3                                                   88
+
+#define  _BTN_语言设置_BUTTON1                                                     1
+
+#define  _BTN_语言设置_BUTTON2                                                     2
+
+#define  _RTC_语言设置_RTC1                                                        4
+
+//全局图片language-按下.jpg
+#define  _IMG_LANGUAGE-按下_JPG                                                 11
+
+#define  _BTN_语言设置_BUTTON3                                                     3
+
+#define  _RTC_清除所有数据警告_RTC1                                                   26
+
+#define  _BTN_清除所有数据警告_BUTTON1                                                 4
+
+#define  _BTN_清除所有数据警告_BUTTON2                                                 5
+
+#define  _BTN_帮助-关于我们_BUTTON1                                                  3
+
+#define  _RTC_帮助-关于我们_RTC1                                                     4
+
+#define  _BTN_帮助-关于我们_BUTTON3                                                 10
+
+#define  _BTN_帮助-关于我们_BUTTON4                                                 11
+
+#define  _RTC_内存溢出警告_RTC1                                                      3
+
+#define  _BTN_内存溢出警告_BUTTON1                                                   4
+
+#define  _BTN_内存溢出警告_BUTTON2                                                   5
+
+#define  _PROGRESS_帮助-帮助文档_PROGRESS1                                          13
+
+#define  _SLIDER_帮助-帮助文档_SLIDER1                                              14
+
+#define  _VIDEOPLAYER_帮助-帮助文档_VIDEOPLAYER1                                     1
+
+#define  _RTC_帮助-帮助文档_RTC1                                                     4
+
+#define  _BTN_帮助-帮助文档_BUTTON3                                                  9
+
+#define  _BTN_帮助-帮助文档_BUTTON4                                                 10
+
+//全局图片help-guide1.2按下.jpg
+#define  _IMG_HELP-GUIDE1.2按下_JPG                                             18
+
+#define  _BTN_帮助-帮助文档_BUTTON5                                                 11
+
+#define  _BTN_帮助-帮助文档_BUTTON6                                                 12
+
+#define  _BTN_帮助-帮助文档_BUTTON1                                                  2
+
+#define  _BTN_帮助-帮助文档_BUTTON2                                                  3
+
+#define  _RTC_帮助-联系我们_RTC1                                                     4
+
+#define  _BTN_帮助-联系我们_BUTTON1                                                  2
+
+#define  _BTN_帮助-联系我们_BUTTON3                                                  3
+
+#define  _BTN_帮助-联系我们_BUTTON2                                                  9
+
+#define  _RTC_清除单次数据警告_RTC1                                                   26
+
+#define  _BTN_清除单次数据警告_BUTTON1                                                 4
+
+#define  _BTN_清除单次数据警告_BUTTON2                                                 5
+
+//画面数据导出提示界面的背景图片
+#define  _IMG_数据导出提示界面                                                        20
+
+#define  _RTC_数据导出提示界面_RTC1                                                    2
+
+#define  _TXT_DIS__数据导出提示界面_TEXT_DISPLAY1                                      4
+
+#define  _BTN_数据导出提示界面_BUTTON1                                                 5
+
+#define  _BTN_数据导出提示界面_BUTTON2                                                 6
+
